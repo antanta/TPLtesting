199ec31f1a8282a1dd659ef19db87a579842647f
Angel Nikolov <angel.nikolov@bulpros.com>
2017-09-19 10:46:56 +0300

    TPL project created

---
+++ TPLtesting/ParallelTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPLtesting
+{
+    class ParallelTest
+    {
+        static void Main(string[] args)
+        {
+            SimpleTest();
+            return;
+        }
+
+        static void SimpleTest()
+        {
+            int RetCode = 0;
+            Task RedistributionTask = new Task(() => RetCode = Timed_Message("Five ", 4));
+            RedistributionTask.Start();
+            Task AltRedistributionTask = new Task(() => RetCode = Timed_Message("Three ", 2));
+            AltRedistributionTask.Start();
+            Timed_Message("Main", 6);
+
+            // wait for input before exiting
+            Console.WriteLine("Press enter to finish after both [Complete] messages appear.");
+            Console.ReadLine();
+        }
+
+        static void Listing1()
+        {
+            Task.Factory.StartNew(async () => {
+                await Task.Delay(4000);
+                Console.WriteLine("Hello World");
+
+            });
+
+
+
+            // wait for input before exiting
+
+            Console.WriteLine("Main method complete. Press enter to finish.");
+
+            Console.ReadLine();
+        }
+
+        static void Listing2()
+        {
+            // use an Action delegate and a named method
+
+            Task task1 = new Task(new Action(printMessage));
+
+
+
+            // use a anonymous delegate
+
+            Task task2 = new Task(delegate {
+
+                printMessage();
+
+            });
+
+
+
+            // use a lambda expression and a named method
+
+            Task task3 = new Task(() => printMessage());
+
+
+
+            // use a lambda expression and an anonymous method
+
+            Task task4 = new Task(() => {
+
+                printMessage();
+
+            });
+
+
+
+            task1.Start();
+
+            task2.Start();
+
+            task3.Start();
+
+            task4.Start();
+
+
+
+            // wait for input before exiting
+
+            Console.WriteLine("Main method complete. Press enter to finish.");
+
+            Console.ReadLine();
+        }
+
+        static void Listing3()
+        {
+
+        }
+
+        private static int Timed_Message(String arg_Message, int arg_Interval)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Source {0} - Cycle {1} for Interval {2}", arg_Message, i, arg_Interval);
+                Thread.Sleep(1000 * arg_Interval);
+            }
+
+            Console.WriteLine("{0} - Complete", arg_Message);
+            return 0;
+        }
+
+        static void printMessage()
+        {
+
+            Console.WriteLine("Hello World");
+
+        }
+    }
+}
---
+++ TPLtesting/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TPLtesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filePath = @"C:\Projects\test.txt";
+
+            int i = 10;
+            i++;
+
+            ReadFileAsync(filePath);
+
+            Console.ReadLine();
+        }
+
+        public static async Task<int> ReadFileAsync(string filePath)
+        {
+            FileStream fs = new FileStream(filePath, FileMode.Open);
+
+            var buffer = new byte[fs.Length];
+
+            Task<int> readTask = fs.ReadAsync(buffer, 0, buffer.Length);
+
+            // Do some other activity
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Other activitiy finish");
+
+            int length = await readTask;
+
+            Console.WriteLine("Await completed");
+
+            return length;
+        }
+
+        //static void Main(string[] args)
+        //{
+        //    Method();
+        //    Console.WriteLine("Main Thread");
+        //    Console.ReadLine();
+        //}
+
+        //public static void Method()
+        //{
+        //    Task.Run(new Action(LongTask));
+        //    Console.WriteLine("New Thread");
+        //}
+
+        //public static void LongTask()
+        //{
+        //    Thread.Sleep(20000);
+        //}
+    }
+}
